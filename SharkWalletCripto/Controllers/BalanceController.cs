e080bc8cc0c5d4215824249eaeabe500a17f3267
Tupac Rodriguez <tupacrojo@live.com>
2022-06-23 10:42:26 -0300

    Controladores basicos

--- SharkWalletCripto/Controllers/BalanceController.cs
+++ SharkWalletCripto/Controllers/BalanceController.cs
@@ -1,9 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Core.Wallet.Entities;
+using API.Uses.Cases.UOWork;
+using Microsoft.AspNetCore.Mvc;
 namespace SharkWalletCripto.Controllers
 {
-    public class BalanceController : Controller
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BalanceController : ControllerBase
     {
         
+        private readonly IUOWork _context;
+
+        public BalanceController(IUOWork context)
+        {
+            _context = context;
+        }
+
+        //GET
+        //-----------------------------------------------------------
+        /// <summary>
+        /// Api para seleccionar todos los clientes de la base de datos.
+        /// </summary>
+        /// <param name="Balance">Estructura</param>
+        /// <response code="200">Se creo correctamente</response>
+        /// <response code="404">Usuario no encontrado</response>
+        /// <response code="500">Oops! No se pudo buscar el User</response>
+        [HttpGet]
+        public ActionResult<IEnumerable<Balance>> Get()
+        {
+            var entidad = _context.BalanceRepo.GetAll();//llama a todos los registros activos
+            return Ok(entidad);
+        }
+
+        //POST
+        //-----------------------------------------------------------
+        /// <summary>
+        /// cargo un regitro en a tabla clientes
+        /// </summary>
+        /// <param name="User">Estructura</param>
+        /// <response code="200">Se creo correctamente</response>
+        /// <response code="404">Usuario no encontrado</response>
+        /// <response code="500">Oops! No se pudo buscar el User</response>
+        [HttpPost]
+        public ActionResult Post([FromBody] Balance balance)
+        {
+            _context.BalanceRepo.Insert(balance);//agrega un registro
+            _context.Save();
+            return Ok();
+        }
+
+        //DELETE
+        //-----------------------------------------------------------
+        /// <summary>
+        ///  borra registro de la base de datos.
+        /// </summary>
+        /// <param name="id" example="123">UserID</param>
+        /// 
+        /// <response code="200">Se creo correctamente</response>
+        /// <response code="404">Usuario no encontrado</response>
+        /// <response code="500">Oops! No se pudo buscar el User</response>
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var entity = _context.BalanceRepo.GetById(id);
+
+            if (entity == null)
+            {
+                return NotFound("No Se Encontro El Registro Solicitado");
+            }
+            else
+            {
+                _context.BalanceRepo.Delete(id);//borra el registro
+                _context.Save();
+            }
+
+
+            return Ok();
+
+
+        }
     }
-    
 }
+
+
